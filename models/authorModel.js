ed705036992fb1a6bdc26026af7d79cf3ff9891d
AdaN6 <a_disney_da@hotmail.com>
2022-12-07 19:30:36 +0100

    created model

---
+++ models/authorModel.js
@@ -0,0 +1,21 @@
+const {Schema, model} = require('mongoose');
+
+const authorSchema = new Schema({
+    firstName: String,
+    lastName: {
+        type: String,
+        required: true,
+        unique: true,
+        match:/[A-Za-z]/,
+        minlength: 3,
+        maxlength: 20,
+        default: ''
+    },
+    dateOfBirth: Date,
+    active: {
+        type: Boolean,
+        default: true,
+    }
+})
+
+module.exports = model('Author', authorSchema);
