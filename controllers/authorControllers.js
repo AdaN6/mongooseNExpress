6e473a2aa0fe383da524d8a823982f72f870cdd3
AdaN6 <a_disney_da@hotmail.com>
2022-12-07 19:51:21 +0100

    installed mongo sanitizer

--- controllers/authorControllers.js
+++ controllers/authorControllers.js
@@ -1,6 +1,28 @@
-const getAuthors = async (req, res) => {};
+const AuthorCollection = require('../models/authorModel');
 
-const getAuthorById = async (req, res) => {};
+const getAuthors = async (req, res) => {
+    try{
+      // Then we can query our database! This function is meant to return all authors in the database
+      const authors = await AuthorCollection.find();
+      // And we return to our client the query in JSON format.
+      res.status(200).json(authors);
+    } catch (error) {
+      // If there is an error, we will send a 500 status code and the error's message to the client
+      res.status(500).send(error.message);
+    }
+};
+
+const getAuthorById = async (req, res) => {
+    try{
+        const {id} = req.params;
+        const authorById = await AuthorCollection.findById(id);
+        if (authorById) return res.status(200).json(authorById);
+        res.status(404).send('Author not found');
+
+    } catch (error){
+    res.status(500).send(error.message);
+    }
+};
 
 const postAuthor = (req, res) => {};
 
--- index.js
+++ index.js
@@ -10,6 +10,11 @@ connectToDB();
 
 const port = process.env.PORT || 6000;
 
+//installed express-mongo-sanitize, then require it
+const sanitize = require('express-mongo-sanitize');
+// Then apply it to our app (you can also pass arguments to tailor your sanitization further)
+app.use(sanitize({allowsDots: true, replaceWith: "_"}))
+
 app.use('/authors', authorRouter);
 
 app.listen(port, ()=> console.log(`Server started on port ${6000}`));
